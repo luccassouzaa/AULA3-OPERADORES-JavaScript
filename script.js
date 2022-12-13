b5fc2a0384474946e63f8e1f24234492e9527a97
Luccas Andre <luccasmelim@yahoo.com.br>
2022-12-13 18:49:50 -0300

    v2.0

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //OPERADORES
 
+//Variaveis
+
+var x = 10, y = 2
+
+var resultadoSoma = x + y
+var resultadoSubtracao = x - y
+var resultadoDivisao = x / y
+var resultadoMultiplicacao = x * y
+var resultadoResto = x % y
+
+//Binario
+    //Resultado
+    console.log(resultadoSoma)
+    console.log(resultadoSubtracao)
+    console.log(resultadoDivisao)
+    console.log(resultadoMultiplicacao)
+    console.log(resultadoResto)
+
+//Unario
+    console.log(x++) //Mostra o valor, depois incrementa
+    console.log(++x) //Incrementa e mostra o valor ja incrementado
+
+//Prioridades: Multiplicação, Divisão, Soma, Subtração
